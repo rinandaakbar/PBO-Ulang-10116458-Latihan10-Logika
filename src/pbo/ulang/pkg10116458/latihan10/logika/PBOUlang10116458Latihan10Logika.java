86a7057a562bede28b10a45fade0d463c6b479f3
nanda <nanda@nanda>
2019-09-26 22:09:37 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan10/logika/PBOUlang10116458Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan10.logika;
+
+/**
+ *
+ * @author nanda
+ */
+public class PBOUlang10116458Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
